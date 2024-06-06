3ce9bc6c027a022bc2d4b142941a6c1a5178bb26
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2024-06-06 15:05:19 +0530

    refactor: Separate temperature and humidity logging into functions and update LCD cursor position in update_lcd_temperature function

--- src/main.cpp
+++ src/main.cpp
@@ -28,9 +28,52 @@ void setup()
   lcd.print("Network");
   delay(500);
 
-  delayMS = setup_serial_info_for_dht_and_get_min_delay(dht);
+  Serial.begin(9600);
+  // Initialize device.
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("%"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("%"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
   // Set delay between sensor readings based on sensor details.
-  delayMS = delayMS / 1000;
+  delayMS = sensor.min_delay / 1000;
 }
 
 void loop()
@@ -39,6 +82,8 @@ void loop()
 
   // Get temperature event and print its value.
   sensors_event_t event;
+  float new_temperature;
+  float new_humidity;
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature))
   {
@@ -46,10 +91,15 @@ void loop()
   }
   else
   {
-    temperature = String(event.temperature);
-    serial_log_temperature(event);
+    if (temperature != String(event.temperature))
+    {
+      temperature = String(event.temperature);
+      serial_log_temperature(event);
+      lcd.setCursor(0, 0);
+      new_temperature = event.temperature;
+      lcd.print("Temperature: " + String(new_temperature) + "C");
+    }
   }
-
   // Get humidity event and print its value.
   dht.humidity().getEvent(&event);
   if (isnan(event.relative_humidity))
@@ -58,9 +108,18 @@ void loop()
   }
   else
   {
-    humidity = String(event.relative_humidity);
-    serial_log_humidity(event);
+    if (humidity != String(event.relative_humidity))
+    {
+      humidity = String(event.relative_humidity);
+      serial_log_humidity(event);
+      new_humidity = event.relative_humidity;
+      lcd.setCursor(0, 1);
+      lcd.print("Humidity: " + String(new_humidity) + "%");
+    }
   }
 
-  update_lcd_temperature(lcd, temperature.toFloat(), humidity.toFloat());
+  if (String(new_temperature) != temperature || String(new_humidity) != humidity)
+  {
+    update_lcd_temperature(lcd, temperature.toFloat(), humidity.toFloat());
+  }
 }
